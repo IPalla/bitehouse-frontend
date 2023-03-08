099f37ef369288654a124b19ceffc641d66ef9d5
Ipoj <nachopallaj@gmail.com>
2023-03-09 00:56:17 +0100

    updated folder

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ cd nginx-hosting
 docker build -t bitehouse-frontend .
 docker run -dp 8080:8080 bitehouse-frontend
 //gcloud build submit
+//ng build --output-path docs --base-href /repository-name/
+//ng build --configuration=development --output-path docs --base-href /bitehouse-frontend/
