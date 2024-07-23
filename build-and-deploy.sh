30272700341edba0daa8ff01665faa3dc8e9ccc4
Ipoj <nachopallaj@gmail.com>
2024-07-23 17:38:30 +0200

    TMP commit

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,3 +1,10 @@
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+echo latest
 docker image rm europe-north1-docker.pkg.dev/bite-house-burger/bhb-repository/bitehouse-frontend:latest --force
 ng build --configuration=development
 rm -r nginx-hosting/dist
